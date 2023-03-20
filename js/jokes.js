24c5c8ae871c638a473f5410960d48ce96aa2fd5
Malynna Yang <111911988+malynna-yang@users.noreply.github.com>
2023-03-20 17:02:19 +0000

    Mon, Mar 20, 2023, 10:02 AM -07:00

---
+++ js/jokes.js
@@ -0,0 +1,13 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokebtn.addEventListener('click', generateJoke)
+
+async function generateJoke(){
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json',},
+    })
+
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
