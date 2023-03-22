174d863a2d9ca12fe10a5a04a004c1bddcbfabca
Malynna Yang <111911988+malynna-yang@users.noreply.github.com>
2023-03-22 16:35:50 +0000

    Wed, Mar 22, 2023, 9:35 AM -07:00

---
+++ js/colorboard.js
@@ -0,0 +1,19 @@
+const container = document.getElementByID('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
+for(let i = 0; 1 < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    container.appendChild(square)
+    square.addEventListener('mouseover', () => setColor(square))
+}
+
+
+function setColor(element) {
+    const color = getRandomColor()
+}
+
+function getRandomColor () {
+    return colors[Math.floor(Math.random() * 5)]
+}
