49a2ad6142f1a966116ec48ca6ace5981141a235
Malynna Yang <111911988+malynna-yang@users.noreply.github.com>
2023-04-14 17:20:32 +0000

    Fri, Apr 14, 2023, 10:20 AM -07:00

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const pg1 = document.querySelectorAll('.button')
 const pg2 = document.querySelectorAll('.button2')
 
 btn1.addEventListener('click', () => {
-    
+    CSStyleDeclaration.removeProperty('z-index')
 })
 
 btn2.addEventListener('click', () => {
