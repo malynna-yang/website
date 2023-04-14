4712f43540de477995786582c7758423d1c5d411
Malynna Yang <111911988+malynna-yang@users.noreply.github.com>
2023-04-14 17:31:06 +0000

    Fri, Apr 14, 2023, 10:31 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-const btn1 = document.getElementById('btn1')
-const btn2 = document.getElementById('btn2')
+const btn1 = document.getElementById('1')
+const btn2 = document.getElementById('2')
 const pg1 = document.querySelectorAll('.button')
 const pg2 = document.querySelectorAll('.button2')
 
+console.log(pg1)
+
 btn1.addEventListener('click', () => {
     console.log('click')
-    btn1.style.zIndex = "1";
+    pg1.style.zIndex = "1";
 })
 
 btn2.addEventListener('click', () => {
