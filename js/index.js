836c80bf7e0435a0aea47d5260d9fac7aa3eb238
Malynna Yang <111911988+malynna-yang@users.noreply.github.com>
2023-04-24 15:46:58 +0000

    Mon, Apr 24, 2023, 8:46 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 const btn1 = document.getElementById('1')
 const btn2 = document.getElementById('2')
-const pg1 = document.querySelectorAll('.button')
-const pg2 = document.querySelectorAll('.button2')
-
-pg2.style.backgroundColor = 'green'
+const pg1 = document.querySelector('.button')
+const pg2 = document.querySelector('.button2')
 
 
 btn1.addEventListener('click', () => {
     console.log('click')
-    pg1.style.backgroundColor = "green";
+    pg1.style.zIndex = "1";
+    pg2.style.zIndex = "-1";
 })
 
 btn2.addEventListener('click', () => {
-
+    pg2.style.zIndex =  "1";
+    pg1.style.zIndex = "0";
 })
